e34426248338c5c0e24f054f9975f2ead8240bea
pbborel <pbborel@vente-privee.com>
2019-04-24 10:08:28 +0200

    Set postgre info in docker-compose

--- src/app/db/index.js
+++ src/app/db/index.js
@@ -1,20 +1,12 @@
 const { Pool, Client } = require("pg");
-const connectionString = "postgres://user:password@127.0.0.1:5432/dockerDb";
+const cString = "postgres://postgres:PASSWORD@postgres/postgres";
 
 const pool = new Pool({
-  user: "user",
-  host: "127.0.0.1",
-  database: "dockerDb",
-  password: "password",
-  port: 5432
+  connectionString: cString
 });
 
 const client = new Client({
-  user: "user",
-  host: "127.0.0.1",
-  database: "dockerDb",
-  password: "password",
-  port: 5432
+  connectionString: cString
 });
 
 const createBd = async () => {
@@ -22,14 +14,15 @@ const createBd = async () => {
   const createTableText = `
     CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-    CREATE TEMP TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    data JSONB
+    CREATE TABLE IF NOT EXISTS users (
+      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      data JSONB
     );
   `;
 
   await client.connect();
   // create our temp table
+
   await client.query(createTableText);
 
   console.log("Db created");
--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,8 +1,29 @@
-const users = require("./users");
+const Router = require("express-promise-router");
+const db = require("../db");
+const faker = require("faker");
+faker.locale = "fr";
+
 module.exports = app => {
   app.get("/", function(req, res) {
     res.send("Hello World!");
   });
 
-  app.use("/users", users);
+  app.get("/users", async (req, res) => {
+    const { rows } = await db.query("SELECT * FROM users");
+    res.send(rows);
+  });
+
+  app.get("/users/add", async (req, res) => {
+    const newUser = {
+      name: faker.name.findName(),
+      email: faker.internet.email()
+    };
+    console.log("newUser", newUser);
+
+    // create a new user
+    await db.query("INSERT INTO users(data) VALUES($1)", [newUser]);
+
+    const { rows } = await db.query("SELECT * FROM users");
+    res.send(rows);
+  });
 };
